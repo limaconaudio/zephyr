a7d57da0cac5bf14cd7fd91fc5b1041c4ac7ef10
Limacon Audio Ltd <33643330+limaconaudio@users.noreply.github.com>
2018-10-02 11:33:49 +0100

    Update uart_spinalhdl.c

--- drivers/serial/uart_spinalhdl.c
+++ drivers/serial/uart_spinalhdl.c
@@ -1,10 +1,9 @@
 /*
- *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 /**
- * @brief UART driver for the SpinalHDL SoC
+ * @brief UART driver for the SPINALHDL SoC
  */
 
 #include <kernel.h>
@@ -12,26 +11,32 @@
 #include <uart.h>
 #include <board.h>
 
-#define RXDATA_MASK    	0xFF       /* Receive Data Mask */
-#define RX_FULL    	(1 << 8)   /* Receive FIFO Full */
-#define RX_EMPTY	(1 << 9)   /* Receive FIFO Empty */
-#define RX_ENB    	(1 << 10)  /* Receive Enable */
+#define TX_AVAIL	(0x00FF0000)
+#define RX_EMPTY	(0xFF000000)
+#define DATA_MASK	0xFF
+#define DATA_VALID	(1 << 16)
+#define DATA_LEN8	(0x7F)
+#define NO_PARITY	0x00
+#define STOP_BIT1	(1 << 16)
 
-#define TXDATA_MASK    	0xFF       /* Transmit Data Mask */
-#define TX_FULL    	(1 << 8)   /* Transmit FIFO Full */
-#define TX_EMPTY	(1 << 9)   /* Transmit FIFO Empty */
-#define TX_ENB    	(1 << 10)  /* Transmit Enable */
+/* device config */
+struct uart_spinalhdl_config {
+        struct uart_device_config uconf;
+        /* Baud rate */
+        u32_t baudrate;
+};
 
 struct uart_spinalhdl_regs_t {
-	u32_t tx;
-	u32_t rx;
+	u32_t data;
+	u32_t status;
+	u32_t clk_div;
+	u32_t cfg;
 };
 
 #define DEV_CFG(dev)                                            \
-        ((const struct uart_device_config * const)	        \
-         (dev)->config->config_info)
+        ((struct uart_spinalhdl_config *) (dev)->config->config_info)
 #define DEV_UART(dev)                                           \
-        ((struct uart_spinalhdl_regs_t *)(DEV_CFG(dev))->base)
+        ((struct uart_spinalhdl_regs_t *)(DEV_CFG(dev))->uconf.base)
 
 /**
  * @brief Output a character in polled mode.
@@ -49,9 +54,9 @@ static unsigned char uart_spinalhdl_poll_out(struct device *dev,
 	volatile struct uart_spinalhdl_regs_t *uart = DEV_UART(dev);
 	
 	/* Wait till TX_FIFO gets empty */
-	while (uart->tx & TX_FULL);
+	while (uart->status & TX_AVAIL);
 
-	uart->tx = (int)c;
+	uart->data = (int)c;
 
 	return c;
 }
@@ -62,32 +67,37 @@ static unsigned char uart_spinalhdl_poll_out(struct device *dev,
  * @param dev UART device struct
  * @param c Pointer to character
  *
- * @return 0 if a character arrived, -1 if the input buffer if empty.
+ * @return 0 if a character arrived, -1 if the input buffer if empty or
+ *           the data is not valid.
  */
 static int uart_spinalhdl_poll_in(struct device *dev, unsigned char *c)
 {
 	volatile struct uart_spinalhdl_regs_t *uart = DEV_UART(dev);
 	
-	u32_t val = uart->rx;
+	u32_t status = uart->status;
+	u32_t data = uart->data;
 
-	if (val & RX_EMPTY)
+	/* Check for FIFO empty */
+	if (!(status & RX_EMPTY))
 		return -1;
 
-	*c = (unsigned char)(val & RXDATA_MASK);
+	/* Check for data validity */
+	if (!(data & DATA_VALID))
+		return -1;
+
+	*c = (unsigned char)(data & DATA_MASK);
 
 	return 0;
 }
 
 static int uart_spinalhdl_init(struct device *dev)
 {
+	volatile struct uart_spinalhdl_config *cfg = DEV_CFG(dev);
 	volatile struct uart_spinalhdl_regs_t *uart = DEV_UART(dev);
 
-	/* Enable TX and RX channels */
-	uart->tx |= TX_ENB;
-	uart->rx |= RX_ENB;
-
-	/* Set baud rate */
-//	uart->div = cfg->sys_clk_freq / cfg->baud_rate - 1;
+	/* Config UART */
+	uart->clk_div = ((SPINALHDL_FCLK_RATE / (cfg->baudrate * 8)) - 1) & 0xFFFFF;
+	uart->cfg = (DATA_LEN8 + NO_PARITY + STOP_BIT1);
 
 	return 0;
 }
@@ -98,9 +108,12 @@ static const struct uart_driver_api uart_spinalhdl_driver_api = {
 	.err_check        = NULL,
 };
 
-static const struct uart_device_config uart_spinalhdl_dev_cfg = {
-	.base = (void *)SPINALHDL_UART_BASE_ADDR,
-	.sys_clk_freq = CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC,
+static const struct uart_spinalhdl_config uart_spinalhdl_dev_cfg = {
+	.uconf = {
+		.base = (void *)SPINALHDL_UART_BASE_ADDR,
+		.sys_clk_freq = CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC,
+	},
+	.baudrate = SPINALHDL_UART_BAUDRATE,
 };
 
 DEVICE_AND_API_INIT(uart_spinalhdl, "uart0",
