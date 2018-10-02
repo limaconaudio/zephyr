d09a24b44eec581db25609ed8001767b3f0687d0
Limacon Audio Ltd <33643330+limaconaudio@users.noreply.github.com>
2018-10-02 11:10:37 +0100

    tidy

--- arch/riscv32/soc/riscv-privilege/spinalhdl/soc.h
+++ arch/riscv32/soc/riscv-privilege/spinalhdl/soc.h
@@ -11,7 +11,7 @@
 
 #include <soc_common.h>
 
-#define SPINALHDL_FCLK_RATE	50000000
+#define SPINALHDL_FCLK_RATE	         50000000
 
 /* Platform Level Interrupt Controller (PLIC) interrupt sources */
 
@@ -22,14 +22,14 @@
 #define SPINALHDL_I2C_IRQ            (RISCV_MAX_GENERIC_IRQ + 4)
 
 /* UART Configuration */
-#define SPINALHDL_UART_BASE_ADDR       0x00030000
-#define SPINALHDL_UART_BAUDRATE        115200
+#define SPINALHDL_UART_BASE_ADDR      0x00030000
+#define SPINALHDL_UART_BAUDRATE       115200
 
 /* GPIO Configuration */
-#define SPINALHDL_GPIO_0_BASE_ADDR       0x00000000
+#define SPINALHDL_GPIO_0_BASE_ADDR    0x00000000
 
 /* Platform Level Interrupt Controller Configuration */
-#define SPINALHDL_PLIC_BASE_ADDR		0x00060000
+#define SPINALHDL_PLIC_BASE_ADDR		  0x00060000
 #define SPINALHDL_PLIC_REG_PRI	(SPINALHDL_PLIC_BASE_ADDR + 0x0C)
 #define SPINALHDL_PLIC_REG_IRQ_EN	(SPINALHDL_PLIC_BASE_ADDR + 0x10)
 #define SPINALHDL_PLIC_REG_THRES	(SPINALHDL_PLIC_BASE_ADDR + 0x14)
@@ -38,11 +38,11 @@
 #define SPINALHDL_PLIC_MAX_PRIORITY	7
 
 /* Timer configuration */
-#define RISCV_MTIME_BASE             0x0005000C
-#define RISCV_MTIMECMP_BASE          0x00050004
+#define RISCV_MTIME_BASE               0x0005000C
+#define RISCV_MTIMECMP_BASE            0x00050004
 
 /* lib-c hooks required RAM defined variables */
-#define RISCV_RAM_BASE               CONFIG_RISCV_RAM_BASE_ADDR
-#define RISCV_RAM_SIZE               CONFIG_RISCV_RAM_SIZE
+#define RISCV_RAM_BASE                 CONFIG_RISCV_RAM_BASE_ADDR
+#define RISCV_RAM_SIZE                 CONFIG_RISCV_RAM_SIZE
 
 #endif /* __RISCV32_SPINALHDL_SOC_H_ */
