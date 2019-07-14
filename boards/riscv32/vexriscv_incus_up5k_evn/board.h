b6a3c1656071e14cbfea189ea0cc5fbd7470c41b
SW Dev <swdev@limacon-audio.com>
2019-07-14 21:50:06 +0530

    boards: Add UP5K Evn board support for Incus SoC
    Add UP5K Evn board support for VexRiscv based Incus SoC.
    
    Signed-off-by: SW Dev <swdev@limacon-audio.com>

---
+++ boards/riscv32/vexriscv_incus_up5k_evn/board.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2019 Limacon Limited.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __INC_BOARD_H
+#define __INC_BOARD_H
+
+#include <soc.h>
+
+
+
+#endif /* __INC_BOARD_H */
